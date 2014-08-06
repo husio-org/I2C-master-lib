36a837655db194426c3c79ff16325f6827d99760
rafael del valle <rvalle@celpax.com>
2014-08-06 10:48:46 +0200

    fixed function definition

--- I2C_master.c
+++ I2C_master.c
@@ -40,7 +40,7 @@ uint8_t I2C_start(uint8_t address){
 	return 0;
 }
 
-void I2C_write(uint8_t data){
+uint8_t I2C_write(uint8_t data){
 	// load data into data register
 	TWDR = data;
 	// start transmission of data
